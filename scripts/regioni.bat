673752940ae75a3cade5cf2914b224be74d9828a
antoniograsso21 <antoniograsso21@gmail.com>
2021-02-21 18:19:27 +0100

    2021/02/21 update

--- scripts/regioni.bat
+++ scripts/regioni.bat
@@ -8,24 +8,24 @@ call %anaconda_dir%\Scripts\activate.bat %anaconda_dir%
 call conda activate covid19
 
 %=Visualization Regions Notebooks=%
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\abruzzo.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\basilicata.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\calabria.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\campania.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\emilia-romagna.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\friuli-venezia-giulia.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\lazio.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\liguria.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\lombardia.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\marche.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\molise.ipynb
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\abruzzo.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\basilicata.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\calabria.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\campania.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\emilia-romagna.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\friuli-venezia-giulia.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\lazio.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\liguria.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\lombardia.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\marche.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\molise.ipynb"
 call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\%notebook_bolzano%"
 call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\%notebook_trento%"
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\piemonte.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\puglia.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\sardegna.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\sicilia.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\toscana.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\umbria.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\valle-d-aosta.ipynb
-call start /b jupyter nbconvert --to notebook --inplace --execute %notebook_root%\veneto.ipynb
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\piemonte.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\puglia.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\sardegna.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\sicilia.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\toscana.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\umbria.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\valle-d-aosta.ipynb"
+call start /b jupyter nbconvert --to notebook --inplace --execute "%notebook_root%\veneto.ipynb"
